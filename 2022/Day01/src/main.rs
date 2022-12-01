ae7c0b46e53edb61d009df9b322b881a245ed5e3
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-12-01 15:22:30 -0600

    Day 1 part 1

---
+++ 2022/Day01/src/main.rs
@@ -0,0 +1,58 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file = fs::read_to_string(args[1].clone()).expect("Could not read to String");
+    let elf_group = Group::parse_input(file);
+    let highest_cal = elf_group.highest_cals();
+    println!("Elf {} has the highest amount of calories stored, with {} total calories.",highest_cal.1+1,highest_cal.0.sum());
+}
+
+#[derive(Clone,Debug,PartialEq)]
+struct Elf {
+    cals: Vec<u64>
+}
+
+impl Elf {
+    fn new(cals:Vec<u64>) -> Self {
+        Self{cals}
+    }
+
+    fn sum(&self) -> u64 {
+        let mut tot = 0;
+        for n in self.cals.iter() {
+            tot+=n;
+        } tot
+    }
+}
+
+#[derive(Debug)]
+struct Group {
+    elves: Vec<Elf>
+}
+
+impl Group {
+    fn parse_input(string:String) -> Self {
+        let mut elves: Vec<Elf> = vec![];
+        let mut vec: Vec<u64> = vec![];
+        for line in string.lines() {
+            match line.is_empty() {
+                false => vec.push(line.parse::<u64>().unwrap()),
+                true => {
+                    let elf = Elf::new(vec.clone());
+                    vec.clear();
+                    elves.push(elf);
+                }
+            }
+        }
+        Group{elves}
+    }
+
+    fn highest_cals(&self) -> (&Elf,usize) {
+        let mut highest = self.elves.get(0).unwrap();
+        for elf in self.elves.iter() {
+            if elf.sum() > highest.sum() {highest = elf}
+        }
+        (highest,self.elves.iter().position(|i| i == highest).unwrap())
+    }
+}
