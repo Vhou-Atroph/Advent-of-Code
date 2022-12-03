602781ccb31032ba52d46535e04638fb3f26c3ab
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-12-02 19:23:42 -0600

    Update main.rs
    Removing unneeded enums

--- 2022/Day02/src/main.rs
+++ 2022/Day02/src/main.rs
@@ -9,18 +9,6 @@ fn main() {
     println!("PART 1:{}\nPART 3:{}",win_count,win_count2)
 }
 
-enum Opponent {
-    A, // Rock
-    B, // Paper
-    C // Scissors
-}
-
-enum MyChoice {
-    X, // Lose
-    Y, // Draw
-    Z // Win
-}
-
 struct StrategyGuide(String);
 
 impl StrategyGuide {
@@ -31,8 +19,8 @@ impl StrategyGuide {
     fn wincount(&self) -> u64 {
         let mut count = 0;
         for line in self.0.lines() {
-            let opp = parse_opponent(&line[0..=0]);
-            let my = parse_my_choice(&line[2..=2]);
+            let opp = &line[0..=0];
+            let my = &line[2..=2];
             count+=eval(opp,my);
         } count
     }
@@ -40,57 +28,41 @@ impl StrategyGuide {
     fn wincount_real(&self) -> u64 {
         let mut count = 0;
         for line in self.0.lines() {
-            let opp = parse_opponent(&line[0..=0]);
-            let my = parse_my_choice(&line[2..=2]);
+            let opp = &line[0..=0];
+            let my = &line[2..=2];
             count+=eval_part_2(opp,my);
         } count
     }
 }
 
-fn parse_opponent(str:&str) -> Opponent {
-    match str {
-        "A" => return Opponent::A,
-        "B" => return Opponent::B,
-        "C" => return Opponent::C,
-        _ => panic!("Unexpected character. Panicking!")
-    }
-}
-
-fn parse_my_choice(str:&str) -> MyChoice {
-    match str {
-        "X" => return MyChoice::X,
-        "Y" => return MyChoice::Y,
-        "Z" => return MyChoice::Z,
-        _ => panic!("Unexpected character. Panicking!")
-    }
-}
-
-fn eval(x:Opponent,y:MyChoice) -> u64 {
+fn eval(x:&str,y:&str) -> u64 {
     match (x,y) {
-        (Opponent::A,MyChoice::Y) => return 8, // 2 + 6
-        (Opponent::B,MyChoice::Z) => return 9, // 3 + 6
-        (Opponent::C,MyChoice::X) => return 7, // 1 + 6
-        (Opponent::A,MyChoice::X) => return 4, // 1 + 3
-        (Opponent::B,MyChoice::Y) => return 5, // 2 + 3
-        (Opponent::C,MyChoice::Z) => return 6, // 3 + 3
-        (Opponent::A,MyChoice::Z) => return 3, // 3 + 0
-        (Opponent::B,MyChoice::X) => return 1, // 1 + 0
-        (Opponent::C,MyChoice::Y) => return 2, // 2 + 0
+        ("A","Y") => return 8, // 2 + 6
+        ("B","Z") => return 9, // 3 + 6
+        ("C","X") => return 7, // 1 + 6
+        ("A","X") => return 4, // 1 + 3
+        ("B","Y") => return 5, // 2 + 3
+        ("C","Z") => return 6, // 3 + 3
+        ("A","Z") => return 3, // 3 + 0
+        ("B","X") => return 1, // 1 + 0
+        ("C","Y") => return 2, // 2 + 0
+        _ => unreachable!()
         
     }
 }
 
-fn eval_part_2(x:Opponent,y:MyChoice) -> u64 {
+fn eval_part_2(x:&str,y:&str) -> u64 {
     match (x,y) {
-        (Opponent::A,MyChoice::Y) => return 4, // 1 + 3 (rock draw)
-        (Opponent::B,MyChoice::Z) => return 9, // 3 + 6 (scissors win)
-        (Opponent::C,MyChoice::X) => return 2, // 2 + 0 (paper lose)
-        (Opponent::A,MyChoice::X) => return 3, // 3 + 0 (scissors lose)
-        (Opponent::B,MyChoice::Y) => return 5, // 2 + 3 (paper draw)
-        (Opponent::C,MyChoice::Z) => return 7, // 1 + 6 (rock win)
-        (Opponent::A,MyChoice::Z) => return 8, // 2 + 6 (paper win)
-        (Opponent::B,MyChoice::X) => return 1, // 1 + 0 (rock lose)
-        (Opponent::C,MyChoice::Y) => return 6, // 3 + 3 (scissors draw)
+        ("A","Y") => return 4, // 1 + 3 (rock draw)
+        ("B","Z") => return 9, // 3 + 6 (scissors win)
+        ("C","X") => return 2, // 2 + 0 (paper lose)
+        ("A","X") => return 3, // 3 + 0 (scissors lose)
+        ("B","Y") => return 5, // 2 + 3 (paper draw)
+        ("C","Z") => return 7, // 1 + 6 (rock win)
+        ("A","Z") => return 8, // 2 + 6 (paper win)
+        ("B","X") => return 1, // 1 + 0 (rock lose)
+        ("C","Y") => return 6, // 3 + 3 (scissors draw)
+        _ => unreachable!()
         
     }
 }
